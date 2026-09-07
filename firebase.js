87bb80669c4f2063bccdcd3d93bff8e08fc53f0d
abbas561 <akakk19926666@gmail.com>
2026-09-07 21:06:57 +0300

    Refactor Firebase imports and configuration

--- firebase.js
+++ firebase.js
@@ -1,15 +1,50 @@
-import { initializeApp } from "https://www.gstatic.com/firebasejs/12.18.0/firebase-app.js";
-import { getFirestore } from "https://www.gstatic.com/firebasejs/12.18.0/firebase-firestore.js";
+// Firebase connection
+
+import { initializeApp } 
+from "https://www.gstatic.com/firebasejs/12.18.0/firebase-app.js";
+
+import { 
+getFirestore,
+collection,
+getDocs,
+addDoc,
+updateDoc,
+deleteDoc,
+doc
+}
+from "https://www.gstatic.com/firebasejs/12.18.0/firebase-firestore.js";
+
 
 const firebaseConfig = {
-  apiKey: "AIzaSyB6Xqmz-CMzvL0TS0Bn1cmNqmd8QdJVAuA",
-  authDomain: "dhiqar-store.firebaseapp.com",
-  projectId: "dhiqar-store",
-  storageBucket: "dhiqar-store.firebasestorage.app",
-  messagingSenderId: "218844146604",
-  appId: "1:218844146604:web:298a1a028d3c96c7e2a809"
+
+apiKey: "AIzaSyB6Xqmz-CMzvL0TS0Bn1cmNqmd8QdJVAuA",
+
+authDomain: "dhiqar-store.firebaseapp.com",
+
+projectId: "dhiqar-store",
+
+storageBucket: "dhiqar-store.firebasestorage.app",
+
+messagingSenderId: "218844146604",
+
+appId: "1:218844146604:web:298a1a028d3c96c7e2a809"
+
 };
+
 
 const app = initializeApp(firebaseConfig);
 
+
 export const db = getFirestore(app);
+
+
+export {
+
+collection,
+getDocs,
+addDoc,
+updateDoc,
+deleteDoc,
+doc
+
+};
